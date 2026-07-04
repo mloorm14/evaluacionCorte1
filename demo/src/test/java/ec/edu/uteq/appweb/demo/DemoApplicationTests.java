737a45236ff5304d8c72f1f91dabc28b0187ebe9
DiegoGarcia2020 <dgarcias2@uteq.edu.ec>
2026-07-04 08:41:15 -0500

    Creacion inicial del proyecto

---
+++ demo/src/test/java/ec/edu/uteq/appweb/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.uteq.appweb.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
